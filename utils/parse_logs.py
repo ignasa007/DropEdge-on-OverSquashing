a5c36bbd1d28470e692ce3ece82344306e4424f6
Jasraj Singh <jasraj001@e.ntu.edu.sg>
2024-06-02 15:29:19 +0530

    script for parsing logs

--- utils/parse_logs.py
+++ utils/parse_logs.py
@@ -1,1 +1,28 @@
-# TODO: write a script to parse the log files
+from collections import defaultdict
+
+
+def parse_logs(fn):
+
+    f = open(fn, 'r')
+    results = {
+        'Training': defaultdict(list),
+        'Validation': defaultdict(list),
+        'Testing': defaultdict(list),
+    }
+    epoch = 0
+
+    for line in f.readlines():
+    
+        line = line.strip()
+    
+        if 'Epoch' in line:
+            epoch = int(line.rsplit(' ', maxsplit=1)[1])
+        elif any((key in line for key in results.keys())):
+            split, metrics = line.split(': ')
+            metrics = metrics.strip().split(', ')
+            metrics = map(lambda x: x.split(' = '), metrics)
+            results[split]['Epoch'].append(epoch)
+            for metric, value in metrics:
+                results[split][metric].append(float(value))
+
+    return dict(results['Training']), dict(results['Validation']), dict(results['Testing'])
