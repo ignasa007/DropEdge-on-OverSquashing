0e2cb4c7b776faf6945725c6588f33dcb0f97d7a
ignasa007 <jasraj001@ntu.edu.sg>
2024-06-15 10:49:41 +0100

    made an exp file

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+
+task='Node-C'
+for dataset in {'Cora','CiteSeer','PubMed'}; do
+    for gnn in {'GCN','GAT','APPNP'}; do
+        for prob in $(seq 0.1 0.1 0.9); do
+            python -B main.py \
+                --dataset $dataset \
+                --gnn $gnn \
+                --gnn_layer_sizes 128 128 \
+                --attention_heads 2 \
+                --power_iter 10 \
+                --teleport_p 0.1 \
+                --task $task \
+                --ffn_layer_sizes \
+                --dropout $1 \
+                --drop_p $prob \
+                --n_epochs 200 \
+                --learning_rate 0.01 \
+                --weight_decay 0.005 \
+                --test_every 1
+        done    
+    done
+done
+
+
+dataset='QM9'
+task='Graph-R'
+for gnn in {'GCN','GAT','APPNP'}; do
+    for prob in $(seq 0.1 0.1 0.9); do
+        python -B main.py \
+            --dataset $dataset \
+            --gnn $gnn \
+            --gnn_layer_sizes 128 128 \
+            --attention_heads 2 \
+            --power_iter 10 \
+            --teleport_p 0.1 \
+            --task $task \
+            --ffn_layer_sizes \
+            --dropout $1 \
+            --drop_p $prob \
+            --n_epochs 200 \
+            --learning_rate 0.01 \
+            --weight_decay 0.005 \
+            --test_every 1
+    done    
+done
