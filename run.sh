9d2b41563b0568a15174d6e13afca751bdc7d131
ignasa007 <jasraj001@ntu.edu.sg>
2024-08-20 17:36:43 +0000

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-alphas=0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0
 for alpha in $(seq 0.0 1.0 0.1); do
     for sample in $(seq 1 4 1); do
-        python3 -m main.py \
+        python3 -m main \
             --dataset ZINC \
             --alpha $alpha \
             --gnn GCN \
@@ -15,6 +14,6 @@ for alpha in $(seq 0.0 1.0 0.1); do
             --n_epochs 500 \
             --learning_rate 0.001 \
             --weight_decay 0.0005 \
-            --test_every -1
+            --test_every -1;
     done
 done
