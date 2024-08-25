ce62374f844220669e8cb8b191d1620291c5c242
Jasraj Singh <jasraj001@ntu.edu.sg>
2024-08-25 17:49:21 +0100

    dropout into model dir

---
+++ model/dropout/drop_edge.py
@@ -0,0 +1,30 @@
+import torch
+from torch_sparse import SparseTensor
+from torch_geometric.utils import dropout_edge
+from model.dropout.base import BaseDropout
+
+
+class DropEdge(BaseDropout):
+
+    def __init__(self, dropout_prob=0.5):
+
+        super(DropEdge, self).__init__(dropout_prob)
+    
+    def apply_feature_mat(self, x, training=True):
+
+        return super(DropEdge, self).apply_feature_mat(x, training)
+    
+    def apply_adj_mat(self, edge_index, edge_attr=None, training=True):
+
+        if isinstance(edge_index, SparseTensor):
+            row, col, _ = edge_index.coo()
+            edge_index = torch.stack((row, col))
+
+        edge_index, edge_mask = dropout_edge(edge_index, p=self.dropout_prob, training=training)
+        edge_attr = edge_attr[edge_mask] if edge_attr is not None else None
+
+        return edge_index, edge_attr
+    
+    def apply_message_mat(self, messages, training=True):
+
+        return super(DropEdge, self).apply_message_mat(messages, training)
