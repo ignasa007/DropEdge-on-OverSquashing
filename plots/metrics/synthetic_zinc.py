047145b12200ec29e0c665833731aca208e8618d
Jasraj Singh <jasraj001@ntu.edu.sg>
2024-08-29 02:36:52 +0100

    zinc plotting

--- plots/metrics/synthetic_zinc.py
+++ plots/metrics/synthetic_zinc.py
@@ -17,8 +17,8 @@ args.metric = f'Mean {args.metric} Error'
 ps = (0.0, 0.5)
 alphas = list(map(lambda x: round(x, 1), np.arange(0.0, 1.01, 0.1)))
 
-assets_dir = './assets/synthetics/SyntheticZINC/GCN'
-exp_dir = assets_dir.replace('assets', 'results') + '/L=11/P={P}/alpha={alpha}'
+assets_dir = './assets/synthetics/SyntheticZINC'
+exp_dir = assets_dir.replace('assets', 'results') + '/P={P}/alpha={alpha}'
 
 ### RETRIEVE METRICS ###
 
@@ -29,7 +29,9 @@ gap_metrics = defaultdict(list)
 for p in ps:
     for alpha in alphas:
         exp_dir_format = exp_dir.format(P=p, alpha=alpha)
-        for sample_dir in os.listdir(exp_dir_format):
+        for sample, sample_dir in enumerate(os.listdir(exp_dir_format), 1):
+            if int(sample) == 6:
+                continue
             train, val, test = parse_metrics(f'{exp_dir_format}/{sample_dir}/logs')
             if len(train[args.metric]) != 250:
                 continue
@@ -50,19 +52,20 @@ mean_colors = iter(('purple', 'yellow'))
 for p in ps:
     color = next(colors)
     mean_color = next(mean_colors)
-    means, lower, upper = list(), list(), list()
+    means, stds = list(), list()
     for alpha in alphas:
         metrics = train_metrics.get((p, alpha), (np.nan, np.nan, np.nan))
-        ax.scatter([alpha]*len(metrics), metrics, color=color)
         means.append(np.mean(metrics))
-        # ax.scatter(alpha, np.mean(metrics), color=mean_color)
+        stds.append(np.std(metrics))
+    means, stds = map(np.array, (means, stds))
     ax.plot(alphas, means, color=color, label=f'P = {p:.1f}')
+    # ax.fill_between(alphas, means-stds, means+stds, color=color, alpha=0.2)
 ax.set_xlabel(r'$\alpha$', fontsize=14)
 ax.set_ylabel(f"Training {''.join(map(lambda x: x[0], args.metric.split()))}", fontsize=14)
 ax.grid()
 
 handles, labels = ax.get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower center', ncol=len(ps), bbox_to_anchor = (0, -0.1, 1, 1))
+fig.legend(handles, labels, fontsize=12, loc='lower center', ncol=len(ps), bbox_to_anchor=(0,-0.07,1,1))
 fig.tight_layout()
 fn = f"./{assets_dir}/{args.which}/{args.metric}/train.png"
 os.makedirs(os.path.dirname(fn), exist_ok=True)
@@ -77,19 +80,20 @@ mean_colors = iter(('purple', 'yellow'))
 for p in ps:
     color = next(colors)
     mean_color = next(mean_colors)
-    means, lower, upper = list(), list(), list()
+    means, stds = list(), list()
     for alpha in alphas:
         metrics = test_metrics.get((p, alpha), (np.nan, np.nan, np.nan))
-        ax.scatter([alpha]*len(metrics), metrics, color=color)
         means.append(np.mean(metrics))
-        # ax.scatter(alpha, np.mean(metrics), color=mean_color)
+        stds.append(np.std(metrics))
+    means, stds = map(np.array, (means, stds))
     ax.plot(alphas, means, color=color, label=f'P = {p:.1f}')
+    # ax.fill_between(alphas, means-stds, means+stds, color=color, alpha=0.2)
 ax.set_xlabel(r'$\alpha$', fontsize=14)
 ax.set_ylabel(f"Testing {''.join(map(lambda x: x[0], args.metric.split()))}", fontsize=14)
 ax.grid()
 
 handles, labels = ax.get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower center', ncol=len(ps), bbox_to_anchor = (0, -0.1, 1, 1))
+fig.legend(handles, labels, fontsize=12, loc='lower center', ncol=len(ps), bbox_to_anchor=(0,-0.07,1,1))
 fig.tight_layout()
 fn = f"./{assets_dir}/{args.which}/{args.metric}/test.png"
 os.makedirs(os.path.dirname(fn), exist_ok=True)
@@ -104,19 +108,20 @@ mean_colors = iter(('purple', 'yellow'))
 for p in ps:
     color = next(colors)
     mean_color = next(mean_colors)
-    means, lower, upper = list(), list(), list()
+    means, stds = list(), list()
     for alpha in alphas:
         metrics = gap_metrics.get((p, alpha), (np.nan, np.nan, np.nan))
-        ax.scatter([alpha]*len(metrics), metrics, color=color)
         means.append(np.mean(metrics))
-        # ax.scatter(alpha, np.mean(metrics), color=mean_color)
+        stds.append(np.std(metrics))
+    means, stds = map(np.array, (means, stds))
     ax.plot(alphas, means, color=color, label=f'P = {p:.1f}')
+    # ax.fill_between(alphas, means-stds, means+stds, color=color, alpha=0.2)
 ax.set_xlabel(r'$\alpha$', fontsize=14)
 ax.set_ylabel(f"Generalization Gap in {''.join(map(lambda x: x[0], args.metric.split()))}", fontsize=14)
 ax.grid()
 
 handles, labels = ax.get_legend_handles_labels()
-fig.legend(handles, labels, loc='lower center', ncol=len(ps), bbox_to_anchor = (0, -0.1, 1, 1))
+fig.legend(handles, labels, fontsize=12, loc='lower center', ncol=len(ps), bbox_to_anchor=(0,-0.07,1,1))
 fig.tight_layout()
 fn = f"./{assets_dir}/{args.which}/{args.metric}/gen-gap.png"
 os.makedirs(os.path.dirname(fn), exist_ok=True)
